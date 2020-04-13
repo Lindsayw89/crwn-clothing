5099932028bd727b3e9b90578268afb935df0c38
lindsay wilson <lwilson@germblast.com>
2020-04-13 16:12:02 -0500

    added shop page, collection preview

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import {Switch, Route, Link} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage=() => (
 
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-
-);
 
 
 function App() {
@@ -18,7 +13,7 @@ function App() {
     <div >
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
