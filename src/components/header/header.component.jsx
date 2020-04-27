6b230ccf5f8dc8f6ef20927421c9e2a5c0e951ed
lindsay wilson <lwilson@germblast.com>
2020-04-27 11:44:58 -0500

    added cartIcon and cropdown to app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,29 +4,34 @@ import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
-const Header = ({currentUser}) => {
+const Header = ({currentUser, hidden}) => {
     return(
         <div className='header' >
 
-        <Link className='logo-container' to ="/"> 
+        <Link className='logo-container' to ='/'> 
         <Logo className='logo'/>
         </Link>
         <div className='options'>
 
             <Link className='option' to ='/shop'>Shop</Link>
             <Link className='option' to ='/contact'>Contact</Link>
-            {currentUser ? <div className='option' onClick={()=>auth.signOut()}>Sign Out </div> 
-            :
-            <Link className='option' to='/signin'>Sign In</Link>
+            {currentUser ? (<div className='option' onClick={()=>auth.signOut()}>Sign Out </div> 
+             ) :
+           ( <Link className='option' to='/signin'>Sign In</Link>)
         }
+        <CartIcon/>
         </div>
+        {hidden ? null : <CartDropdown/>}
 
         </div>
     )
 
 }
-const mapStateToProps = (state) =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart:{hidden}}) =>({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
