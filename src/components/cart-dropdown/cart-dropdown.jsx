de9dd4d20f8dc4d25f5f7f5f9a86e3a841340e0a
lindsay wilson <lwilson@germblast.com>
2020-04-28 13:39:51 -0500

    updated app with reselect, memoization

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {connect } from 'react-redux';
+import {selectCartItemsCount, selectCartItems} from '../../redux/cart/cart.selectors';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item';
 
-const CartDropdown= ()=> (
+
+const CartDropdown= ({cartItems})=> (
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {cartItems.map(cartItem=> (
+                <CartItem key = {cartItem.id} item= {cartItem}/>
+            ))}
+        </div>
             <CustomButton>Go to Checkout</CustomButton>
         </div>
     
 );
+ const mapStateToProps = (state)=> ({
+     cartItems: selectCartItems(state)
+ })
 
-export default CartDropdown;
+
+export default connect(mapStateToProps)(CartDropdown);
